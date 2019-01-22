94d65d410f88e95a4515ac20671ddea907ca22de
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2019-01-22 16:05:29 -0500

    passing specs on binary search tree

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,22 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let startIdx = 0;
+	let endIdx = array.length-1;
+	let middleIdx = Math.floor((array.length-1)/2);
+	while (true && startIdx <= endIdx) {
+		if (array[middleIdx]===target){
+			return true;
+		} else if (array[middleIdx] < target) {
+			startIdx = middleIdx+1;
+			middleIdx = Math.floor((startIdx+endIdx)/2);
+		} else {
+			endIdx = middleIdx-1;
+			middleIdx = Math.floor((startIdx+endIdx)/2);
+		}
+	} return false;
 };
+[1,3,5,7,8,11,12,14] //6
 
 /*
 	EXTRA CREDIT:
